9d777eea61e952f6cb46c58b79f923b95ec8e7f0
Matiss Senkans <matisso95@gmail.com>
2020-04-27 17:09:09 -0600

    more indentation

--- scripts/sim_tester.py
+++ scripts/sim_tester.py
@@ -12,145 +12,145 @@ import yaml
 
 class simTester:
 
-	def __init__(self):
-		self.pub = rospy.Publisher('/command',Command, queue_size=10, latch=True)
-		self.command_msg = Command()
-		self.command_msg.mode = self.command_msg.MODE_ROLL_PITCH_YAWRATE_THROTTLE
-		self.count = 1 # used for initiating time
+    def __init__(self):
+        self.pub = rospy.Publisher('/command',Command, queue_size=10, latch=True)
+        self.command_msg = Command()
+        self.command_msg.mode = self.command_msg.MODE_ROLL_PITCH_YAWRATE_THROTTLE
+        self.count = 1 # used for initiating time
 
-		rospy.Subscriber('/odom',Odometry,self.callback)
+        rospy.Subscriber('/odom',Odometry,self.callback)
 
-		# with open('/home/matiss/demo_ws/src/demo/scripts/demo.yaml','r') as f:
-		# 	param = yaml.safe_load(f)
+        # with open('/home/matiss/demo_ws/src/demo/scripts/demo.yaml','r') as f:
+        #   param = yaml.safe_load(f)
 
-		param = rospy.get_param('~')
+        param = rospy.get_param('~')
 
-		self.mass = param['dynamics']['mass']
-		self.g = param['dynamics']['g']
+        self.mass = param['dynamics']['mass']
+        self.g = param['dynamics']['g']
 
-		self.controller = controller(param) # initiate controller
-		# calculate the equilibrium force
-		self.force_adjust = param['dynamics']['mass']*param['dynamics']['g']/param['controller']['equilibrium_throttle']
-		# plotting variables
-		# self.time = []
-		# self.pos_x_des = []
-		# self.pos_x_actual = []
-		# self.pos_y_des = []
-		# self.pos_y_actual = []
-		# self.pos_z_des = []
-		# self.pos_z_actual = []
+        self.controller = controller(param) # initiate controller
+        # calculate the equilibrium force
+        self.force_adjust = param['dynamics']['mass']*param['dynamics']['g']/param['controller']['equilibrium_throttle']
+        # plotting variables
+        # self.time = []
+        # self.pos_x_des = []
+        # self.pos_x_actual = []
+        # self.pos_y_des = []
+        # self.pos_y_actual = []
+        # self.pos_z_des = []
+        # self.pos_z_actual = []
 
-	def callback(self,msg):
+    def callback(self,msg):
 
-		# for some reason time did not initiate correctly if done
-		# in the __init__ function
-		if self.count == 1:
-			self.start_time = rospy.Time.now()
-			self.count = 0
-		
-		time = rospy.Time.now()
-		time_from_start = time.to_sec() - self.start_time.to_sec() 
-		pose = trajectory_planner(time_from_start)
+        # for some reason time did not initiate correctly if done
+        # in the __init__ function
+        if self.count == 1:
+            self.start_time = rospy.Time.now()
+            self.count = 0
+        
+        time = rospy.Time.now()
+        time_from_start = time.to_sec() - self.start_time.to_sec() 
+        pose = trajectory_planner(time_from_start)
 
-		pos = msg.pose.pose.position
-		# The mekf gives unusual odometry message, the coordinates are different than NED
-		pos = np.array([-1.*pos.y,-1.*pos.z,pos.x])
-		attitude = msg.pose.pose.orientation
-		vel = msg.twist.twist.linear
-		vel = np.array([-1.*vel.y,-1.*vel.z,vel.x])
-		ang_curr = self.euler(attitude)
+        pos = msg.pose.pose.position
+        # The mekf gives unusual odometry message, the coordinates are different than NED
+        pos = np.array([-1.*pos.y,-1.*pos.z,pos.x])
+        attitude = msg.pose.pose.orientation
+        vel = msg.twist.twist.linear
+        vel = np.array([-1.*vel.y,-1.*vel.z,vel.x])
+        ang_curr = self.euler(attitude)
 
-		# plotting variables
-		# self.time.append(time_from_start)
-		# self.pos_x_actual.append(pos[0])
-		# self.pos_x_des.append(pose[0][0])
+        # plotting variables
+        # self.time.append(time_from_start)
+        # self.pos_x_actual.append(pos[0])
+        # self.pos_x_des.append(pose[0][0])
 
-		# self.pos_y_actual.append(pos[1])
-		# self.pos_y_des.append(pose[0][1])
-		# self.pos_z_actual.append(pos[2])
-		# self.pos_z_des.append(pose[0][2])
+        # self.pos_y_actual.append(pos[1])
+        # self.pos_y_des.append(pose[0][1])
+        # self.pos_z_actual.append(pos[2])
+        # self.pos_z_des.append(pose[0][2])
 
-		reference = np.array([pose[0],pose[2],pose[4],pose[1]])
-		state = np.array([pos,vel,ang_curr[2]])
+        reference = np.array([pose[0],pose[2],pose[4],pose[1]])
+        state = np.array([pos,vel,ang_curr[2]])
 
-		control_inputs = self.controller.update(reference,state)
+        control_inputs = self.controller.update(reference,state)
 
-		accel_input = np.array([control_inputs[0],pose[3]+control_inputs[1]])
+        accel_input = np.array([control_inputs[0],pose[3]+control_inputs[1]])
 
-		states = diff_flatness(pose, accel_input, ang_curr,mass=self.mass,g=self.g)
-		# R = states[1]
-		# angle = self.angles(R)
+        states = diff_flatness(pose, accel_input, ang_curr,mass=self.mass,g=self.g)
+        # R = states[1]
+        # angle = self.angles(R)
 
-		force = states[2] / self.force_adjust
-		force = self.controller.saturate(force)
-		roll = states[0]
-		pitch = states[1]
-		yaw_rate = accel_input[1]
+        force = states[2] / self.force_adjust
+        force = self.controller.saturate(force)
+        roll = states[0]
+        pitch = states[1]
+        yaw_rate = accel_input[1]
 
-		self.command_msg.x = roll
-		self.command_msg.y = pitch
-		self.command_msg.z = 0.0 # yaw_rate if variable desired yaw
-		self.command_msg.F = force
-		self.command_msg.ignore = Command.IGNORE_X | Command.IGNORE_Y | Command.IGNORE_Z
-		self.pub.publish(self.command_msg)
+        self.command_msg.x = roll
+        self.command_msg.y = pitch
+        self.command_msg.z = 0.0 # yaw_rate if variable desired yaw
+        self.command_msg.F = force
+        self.command_msg.ignore = Command.IGNORE_X | Command.IGNORE_Y | Command.IGNORE_Z
+        self.pub.publish(self.command_msg)
 
 
-	def update(self):
-		rospy.spin()
+    def update(self):
+        rospy.spin()
 
-	def angles(self,R): # ends up unused for now
-		yaw = np.arctan2(-R[0][1],R[1][1])
-		pitch = np.arctan2(-R[2][0],R[2][2])
-		roll = np.arctan2(R[2][1]*np.cos(pitch),R[2][2])
-		return np.array([roll,pitch,yaw])
+    def angles(self,R): # ends up unused for now
+        yaw = np.arctan2(-R[0][1],R[1][1])
+        pitch = np.arctan2(-R[2][0],R[2][2])
+        roll = np.arctan2(R[2][1]*np.cos(pitch),R[2][2])
+        return np.array([roll,pitch,yaw])
 
-	def euler(self, quat):
-		w = quat.w
-		x = -1.*quat.y
-		y = -1.*quat.z
-		z = quat.x
+    def euler(self, quat):
+        w = quat.w
+        x = -1.*quat.y
+        y = -1.*quat.z
+        z = quat.x
 
-		roll = np.arctan2(2.0 * (w * x + y * z), 1. - 2. * (x * x + y * y))
-		pitch = np.arcsin(2.0 * (w * y - z * x))
-		yaw = np.arctan2(2.0 * (w * z + x * y), 1. - 2. * (y * y + z * z))
-		return np.array([roll,pitch,yaw])
+        roll = np.arctan2(2.0 * (w * x + y * z), 1. - 2. * (x * x + y * y))
+        pitch = np.arcsin(2.0 * (w * y - z * x))
+        yaw = np.arctan2(2.0 * (w * z + x * y), 1. - 2. * (y * y + z * z))
+        return np.array([roll,pitch,yaw])
 
 
 if __name__ == '__main__':
-	rospy.init_node('sim_tester', anonymous=True)
-	sim_tester = simTester()
+    rospy.init_node('sim_tester', anonymous=True)
+    sim_tester = simTester()
 
-	while not rospy.is_shutdown():
-		try:
-			sim_tester.update()
-		except rospy.ROSInterruptException:
-			print("exiting....")
+    while not rospy.is_shutdown():
+        try:
+            sim_tester.update()
+        except rospy.ROSInterruptException:
+            print("exiting....")
 
-	# Plot the states over time and relative to each other
-	# plt.figure(1)
-	# plt.subplot(311)
-	# plt.plot(sim_tester.time,sim_tester.pos_x_actual)
-	# plt.plot(sim_tester.time,sim_tester.pos_x_des)
+    # Plot the states over time and relative to each other
+    # plt.figure(1)
+    # plt.subplot(311)
+    # plt.plot(sim_tester.time,sim_tester.pos_x_actual)
+    # plt.plot(sim_tester.time,sim_tester.pos_x_des)
 
-	# plt.subplot(312)
-	# plt.plot(sim_tester.time,sim_tester.pos_y_actual)
-	# plt.plot(sim_tester.time,sim_tester.pos_y_des)
+    # plt.subplot(312)
+    # plt.plot(sim_tester.time,sim_tester.pos_y_actual)
+    # plt.plot(sim_tester.time,sim_tester.pos_y_des)
 
-	# plt.subplot(313)
-	# plt.plot(sim_tester.time,sim_tester.pos_z_actual)
-	# plt.plot(sim_tester.time,sim_tester.pos_z_des)
+    # plt.subplot(313)
+    # plt.plot(sim_tester.time,sim_tester.pos_z_actual)
+    # plt.plot(sim_tester.time,sim_tester.pos_z_des)
 
-	# plt.figure(2)
-	# plt.subplot(311)
-	# plt.plot(sim_tester.pos_x_actual,sim_tester.pos_y_actual)
-	# plt.plot(sim_tester.pos_x_des,sim_tester.pos_y_des)
+    # plt.figure(2)
+    # plt.subplot(311)
+    # plt.plot(sim_tester.pos_x_actual,sim_tester.pos_y_actual)
+    # plt.plot(sim_tester.pos_x_des,sim_tester.pos_y_des)
 
-	# plt.subplot(312)
-	# plt.plot(sim_tester.pos_x_actual,sim_tester.pos_z_actual)
-	# plt.plot(sim_tester.pos_x_des,sim_tester.pos_z_des)
+    # plt.subplot(312)
+    # plt.plot(sim_tester.pos_x_actual,sim_tester.pos_z_actual)
+    # plt.plot(sim_tester.pos_x_des,sim_tester.pos_z_des)
 
-	# plt.subplot(313)
-	# plt.plot(sim_tester.pos_y_actual,sim_tester.pos_z_actual)
-	# plt.plot(sim_tester.pos_y_des,sim_tester.pos_z_des)
+    # plt.subplot(313)
+    # plt.plot(sim_tester.pos_y_actual,sim_tester.pos_z_actual)
+    # plt.plot(sim_tester.pos_y_des,sim_tester.pos_z_des)
 
-	# plt.show()
+    # plt.show()
--- scripts/traj_planner.py
+++ scripts/traj_planner.py
@@ -1,46 +1,46 @@
 import numpy as np
 
 def trajectory_planner(time, a=2.0, b=1.0, h=5.0,c=0.5):
-	# a is the width of the trajectory. a = 2.0 means that the path will
-	# be 2 meters wide and 4 meters in length
+    # a is the width of the trajectory. a = 2.0 means that the path will
+    # be 2 meters wide and 4 meters in length
 
-	# b is the change in height of the trajectory. b = 1.0 means that
-	# the trajectory will go 1 meter above and 1 meter below the
-	# desired height (h)
+    # b is the change in height of the trajectory. b = 1.0 means that
+    # the trajectory will go 1 meter above and 1 meter below the
+    # desired height (h)
 
-	# h is the middle of the height range of the trajectory
+    # h is the middle of the height range of the trajectory
 
-	# c determines the speed of the trajectory. c = 1.0 means that the
-	# trajectory will go through one rotation in 2*pi seconds. c = 0.5 
-	# means the trajectory will take 4*pi seconds to go through one
-	# rotation
+    # c determines the speed of the trajectory. c = 1.0 means that the
+    # trajectory will go through one rotation in 2*pi seconds. c = 0.5 
+    # means the trajectory will take 4*pi seconds to go through one
+    # rotation
 
-	# Lift up to needed height and hover there
-	if time < 2.0*np.pi/c:
-		pos = np.array([0.0,0.0,0.0])
-		vel = np.array([0.0,0.0,0.0])
-		head = 0.0
-		head_d = 0.0
-		accel = np.array([0.0,0.0,0.0])
-	elif time < 4.0*np.pi/c:
-		pos = np.array([0.0,0.0,-h+0.001*np.sin(c*time)])
-		vel = np.array([0.0,0.0,0.001*c*np.cos(c*time)])
-		head = 0.0
-		head_d = 0.0
-		accel = np.array([0.0,0.0,-0.001*c**2*np.sin(c*time)])
-	else:
-		# final trajectory, yaw is left commented for reference
-		pos = np.array([a/2*np.sin(2*c*time),a*np.sin(c*time),-h+b*np.sin(c*time)])
-		vel = np.array([a*c*np.cos(2*c*time),a*c*np.cos(c*time),b*c*np.cos(c*time)])
-		head = 0.0#np.arctan2(vel[1],vel[0])
-		head_d = 0.0#(-c*np.sin(c*time)*np.cos(2*c*time)+2*c*np.sin(2*c*time)*np.cos(c*time))/((np.cos(c*time))**2 + (np.cos(2*c*time))**2)
-		accel = np.array([-2*c**2*a*np.sin(2*c*time),-a*c**2*np.sin(c*time),-b*c**2*np.sin(c*time)])
+    # Lift up to needed height and hover there
+    if time < 2.0*np.pi/c:
+        pos = np.array([0.0,0.0,200000.0])
+        vel = np.array([0.0,0.0,0.0])
+        head = 0.0
+        head_d = 0.0
+        accel = np.array([0.0,0.0,0.0])
+    elif time < 4.0*np.pi/c:
+        pos = np.array([0.0,0.0,-h])
+        vel = np.array([0.0,0.0,0.0])
+        head = 0.0
+        head_d = 0.0
+        accel = np.array([0.0,0.0,0.0])
+    else:
+        # final trajectory, yaw is left commented for reference
+        pos = np.array([a/2*np.sin(2*c*time),a*np.sin(c*time),-h+b*np.sin(c*time)])
+        vel = np.array([a*c*np.cos(2*c*time),a*c*np.cos(c*time),b*c*np.cos(c*time)])
+        head = 0.0#np.arctan2(vel[1],vel[0])
+        head_d = 0.0#(-c*np.sin(c*time)*np.cos(2*c*time)+2*c*np.sin(2*c*time)*np.cos(c*time))/((np.cos(c*time))**2 + (np.cos(2*c*time))**2)
+        accel = np.array([-2*c**2*a*np.sin(2*c*time),-a*c**2*np.sin(c*time),-b*c**2*np.sin(c*time)])
 
-	# x only direction
-	# 	pos = np.array([a/2*np.sin(2*c*time),0.0,-h+b*np.sin(c*time)])
-	# 	vel = np.array([a*c*np.cos(2*c*time),0.0,b*c*np.cos(c*time)])
-	# 	head = 0.0
-	# 	head_d = 0.0
-	# 	accel = np.array([-2*c**2*a*np.sin(2*c*time),0.0,-b*c**2*np.sin(c*time)])
+    # x only direction
+    #   pos = np.array([a/2*np.sin(2*c*time),0.0,-h+b*np.sin(c*time)])
+    #   vel = np.array([a*c*np.cos(2*c*time),0.0,b*c*np.cos(c*time)])
+    #   head = 0.0
+    #   head_d = 0.0
+    #   accel = np.array([-2*c**2*a*np.sin(2*c*time),0.0,-b*c**2*np.sin(c*time)])
 
-	return np.array([pos, head, vel, head_d, accel])
+    return np.array([pos, head, vel, head_d, accel])
